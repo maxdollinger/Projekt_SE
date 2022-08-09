211a0c31353f6d41bbc61692b08f2e6e0c6341a7
Maximilian Dollinger <maximilian.dollinger@protonmail.com>
2022-08-09 23:12:40 +0200

    removed other auth urls

--- reporting_system/urls.py
+++ reporting_system/urls.py
@@ -1,9 +1,8 @@
-from django.urls import path, include
+from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path("", auth_views.LoginView.as_view(template_name="index.html")),
-    path("accounts/", include("django.contrib.auth.urls")),
     path("team", views.team, name="team")
 ]
