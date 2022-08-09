c2a47b65d5452a90371b24fc5cc1714818f762af
Maximilian Dollinger <maximilian.dollinger@protonmail.com>
2022-08-09 22:34:23 +0200

    login page as index without functionality

--- reporting_system/views.py
+++ reporting_system/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'cms/index.html')
+    return render(request, 'index.html')
+
+def team(request):
+    return render(request, 'cms/team.html')
