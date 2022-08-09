fe1c1d99e4d688491a408d61533cd081cfb21eaf
Maximilian Dollinger <maximilian.dollinger@protonmail.com>
2022-08-09 23:10:55 +0200

    using django form

--- reporting_system/urls.py
+++ reporting_system/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", auth_views.LoginView.as_view(template_name="index.html")),
+    path("accounts/", include("django.contrib.auth.urls")),
     path("team", views.team, name="team")
 ]
