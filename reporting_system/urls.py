384ec5dc52bc81bb6af95c527d17444ffc2723b8
Maximilian Dollinger <maximilian.dollinger@protonmail.com>
2022-08-10 19:10:50 +0200

    login working

--- reporting_system/urls.py
+++ reporting_system/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
+from reporting_system.forms import CustomLoginForm
 
 urlpatterns = [
-    path("", auth_views.LoginView.as_view(template_name="index.html")),
+    path("", auth_views.LoginView.as_view(
+        template_name="index.html",
+        authentication_form=CustomLoginForm), name="Login"),
+    path("manage", views.manage_reportings, name="Manage_reportings"),
     path("team", views.team, name="team")
 ]
--- reporting_system/views.py
+++ reporting_system/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
@@ -6,3 +7,7 @@ def index(request):
 
 def team(request):
     return render(request, 'cms/team.html')
+
+@login_required
+def manage_reportings(request):
+    return render(request, "manage_reports.html")
