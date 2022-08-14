97ea081a766f5dac8cdf00da6dc0d4d5eed76b47
Maximilian Dollinger <maximilian.dollinger@protonmail.com>
2022-08-14 14:13:07 +0200

    urls

--- reporting_system/urls.py
+++ reporting_system/urls.py
@@ -5,9 +5,9 @@ from reporting_system.forms import CustomLoginForm
 
 urlpatterns = [
     path("add", views.add_correction_report, name="add_correction_report"),
-    path("team", views.team, name="Team")
-    path("overview", views.overview, name="Overview"),
-        authentication_form=CustomLoginForm), name="Login"),
+        path("", auth_views.LoginView.as_view(
         template_name="index.html",
-    path("", auth_views.LoginView.as_view(
+        authentication_form=CustomLoginForm), name="Login"),
+    path("overview", views.overview, name="Overview"),
+    path("team", views.team, name="Team")
 ]
