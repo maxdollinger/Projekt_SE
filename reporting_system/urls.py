81347f0e85d85c081b6e77014830dab73da41e7a
Maximilian Dollinger <maximilian.dollinger@protonmail.com>
2022-08-14 14:14:50 +0200

    merge completed

--- reporting_system/urls.py
+++ reporting_system/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
-from reporting_system.forms import CustomLoginForm
+from reporting_system.forms.login_form import CustomLoginForm
 
 urlpatterns = [
     path("add", views.add_correction_report, name="add_correction_report"),
